a7ffc6873fa11c3859c8d4c3545029b4ba7a8372
n8kolenberg <nkolenberg@gmail.com>
2015-07-27 17:05:41 +0100

    Implemented full fullPage.js and started adding Work Experience and fontello fonts. Needs sprucing upa

---
+++ js/cv.js
@@ -0,0 +1,33 @@
+$(function(){
+	
+
+$('#fullpage').fullpage({
+				sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+				anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+				menu: '#menu',
+				scrollingSpeed: 1000,
+				navigation: true,
+				navigationPosition: 'right',
+				navigationTooltips: ['firstSlide', 'secondSlide', 'thirdSlide', 'fourthSlide'],
+
+				//Accessibility
+				keyboardScrolling: true,
+				animateAnchor: true,
+
+				loopBottom: true,
+				controlArrows: true,
+				verticalCentered: true
+			});
+
+
+
+// $('.thumbs').portfolio({
+// 	cols: 4,
+// 	transition: 'fadeIn'
+// }); //End portfolio
+
+
+
+
+
+});//End ready
